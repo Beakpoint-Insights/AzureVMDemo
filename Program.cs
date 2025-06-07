872e4a5364d886c413e2854f513b59413664621d
DmitriyIvanko <dmitriy.ivanko@protonmail.com>
2025-06-07 06:18:31 +0200

    Added config

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 using System.Reflection;
 using Microsoft.Extensions.Caching.Memory;
+using System.Text.Json;
 using OpenTelemetry;
 using OpenTelemetry.Exporter;
 using OpenTelemetry.Resources;
@@ -15,12 +16,16 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         // Create a path to configuration files
-        var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "";
-        var projectPath = Path.GetFullPath(Path.Combine(basePath, Path.Combine("..", "..", "..")));
+        //var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "";
+        //var projectPath = Path.GetFullPath(Path.Combine(basePath, Path.Combine("..", "..", "..")));
+
+        //Console.WriteLine($"Base path: {basePath}");
+        //Console.WriteLine($"Project path: {projectPath}");
+        //Console.WriteLine($"AppContext base directory: {System.AppContext.BaseDirectory}");
         
         // Configuration setup
         builder.Configuration
-            .SetBasePath(projectPath)
+            .SetBasePath(AppContext.BaseDirectory)
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
             .AddEnvironmentVariables();
@@ -34,56 +39,93 @@ public class Program
         IMemoryCache memoryCache = serviceProvider.GetRequiredService<IMemoryCache>();
         HttpClient httpClient = serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
 
+        var attributes = GetVmMetadataAsync(memoryCache, httpClient).GetAwaiter().GetResult();
+
+        // Get Telemetry receiver address and API key
+        var apiKey = builder.Configuration["Beakpoint:Otel:ApiKey"];
+        var url = builder.Configuration["Beakpoint:Otel:Url"] ?? throw new InvalidOperationException("Beakpoint Otel Url is not configured");
+
         // Add OpenTelemetry
-        //builder.Services.AddOpenTelemetry()
-        //    .WithTracing(tracingBuilder => tracingBuilder
-        //        .SetResourceBuilder(ResourceBuilder.CreateDefault()
-        //            .AddAttributes(attributes))
-        //        .AddAspNetCoreInstrumentation(options => {
-        //            options.EnrichWithHttpRequest = (activity, request) => {
-        //                foreach(var attr in attributes) {
-        //                    activity.SetTag(attr.Key, attr.Value);
-        //                }
-        //            };
-        //        })
-        //        .AddHttpClientInstrumentation(options =>{
-        //            options.EnrichWithHttpRequestMessage = (activity, request) => {
-        //                foreach(var attr in attributes) {
-        //                    activity.SetTag(attr.Key, attr.Value);
-        //                }
-        //            };
-        //        })
-        //        .AddAWSInstrumentation()
-        //        .AddOtlpExporter(opts => {
-        //            opts.Protocol = OtlpExportProtocol.HttpProtobuf;
-        //            opts.ExportProcessorType = ExportProcessorType.Simple;
-        //            opts.Endpoint = new Uri(url);
-        //            opts.Headers = $"x-bkpt-key={apiKey}";
-        //        }));
+        builder.Services.AddOpenTelemetry()
+            .WithTracing(tracingBuilder => tracingBuilder
+                .SetResourceBuilder(ResourceBuilder.CreateDefault()
+                    .AddAttributes(attributes))
+                .AddAspNetCoreInstrumentation(options => {
+                    options.EnrichWithHttpRequest = (activity, request) => {
+                        foreach(var attr in attributes) {
+                            activity.SetTag(attr.Key, attr.Value);
+                        }
+                    };
+                })
+                .AddHttpClientInstrumentation(options =>{
+                    options.EnrichWithHttpRequestMessage = (activity, request) => {
+                        foreach(var attr in attributes) {
+                            activity.SetTag(attr.Key, attr.Value);
+                        }
+                    };
+                })
+                .AddOtlpExporter(opts => {
+                    opts.Protocol = OtlpExportProtocol.HttpProtobuf;
+                    opts.ExportProcessorType = ExportProcessorType.Simple;
+                    opts.Endpoint = new Uri(url);
+                    opts.Headers = $"x-bkpt-key={apiKey}";
+                }));
+
+        builder.WebHost.ConfigureKestrel(serverOptions =>
+        {
+            serverOptions.Listen(System.Net.IPAddress.Parse("0.0.0.0"), 5227);
+        });
         
         var app = builder.Build();
 
         app.MapGet("/", () => {
-            var metadata = GetVmMetadataAsync(memoryCache, httpClient).GetAwaiter().GetResult();
-            return metadata;
+            var attributes = GetVmMetadataAsync(memoryCache, httpClient).GetAwaiter().GetResult();
+            return attributes;
         });
 
         app.Run();
     }
 
-    public static async Task<Dictionary<string, object>?> GetVmMetadataAsync(IMemoryCache memoryCache, HttpClient httpClient)
+    public static async Task<Dictionary<string, object>> GetVmMetadataAsync(IMemoryCache memoryCache, HttpClient httpClient)
     {
-        if (memoryCache.TryGetValue("vm_metadata", out Dictionary<string, object>? cachedMetadata) && cachedMetadata != null)
+        string metadata;
+        if (memoryCache.TryGetValue("vm_metadata", out string? cachedMetadata) && cachedMetadata != null)
         {
-            return cachedMetadata;
+            metadata = cachedMetadata;
+        }
+        else {
+            httpClient.DefaultRequestHeaders.Add("Metadata", "true");
+            var IMDSResponse = await httpClient.GetAsync("http://169.254.169.254/metadata/instance?api-version=2021-02-01");
+            IMDSResponse.EnsureSuccessStatusCode();
+
+            metadata = await IMDSResponse.Content.ReadAsStringAsync();
+
+            memoryCache.Set("vm_metadata", metadata);
         }
 
-        httpClient.DefaultRequestHeaders.Add("Metadata", "true");
-        var response = await httpClient.GetAsync("http://169.254.169.254/metadata/instance?api-version=2020-09-01");
-        response.EnsureSuccessStatusCode();
+        var attributes = new Dictionary<string, object>();
+        var imds = JsonSerializer.Deserialize<JsonElement>(metadata);
+        var compute = imds.GetProperty("compute");
+        
+        string? location = compute.GetProperty("location").GetString(); // ArmRegionName
+        string? size = compute.GetProperty("vmSize").GetString(); // ArmSkuName
+        string? os = compute.GetProperty("osType").GetString(); // ProductName (partially)
+        string? priority = compute.GetProperty("priority").GetString(); // MeterName (partially)
 
-        var metadata = await response.Content.ReadFromJsonAsync<Dictionary<string, object>>();
-        memoryCache.Set("vm_metadata", metadata, TimeSpan.FromMinutes(5)); // Cache for 5 minutes
-        return metadata;
+        attributes["azure.vm.service_name"] = "Virtual Machines";
+        if (location != null) {
+            attributes["azure.vm.location"] = location;
+        }
+        if (size != null) {
+            attributes["azure.vm.size"] = size;
+        }
+        if (os != null) {
+            attributes["azure.vm.os"] = os;
+        }
+        if (priority != null) {
+            attributes["azure.vm.priority"] = priority;
+        }
+
+        return attributes;
     }
 }
